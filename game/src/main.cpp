46ed15d179c10476fa656eb81b0a74d289eed5a0
CamjetAwesome <cameronjettcook@gmail.com>
2025-09-04 11:56:31 -0400

    Added Shapes and Colors
    I like to play and draw smile :)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,26 +2,56 @@
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "game.h"
 
 const unsigned int TARGET_FPS = 50;
 float time = 0;
+float dt = 0;
+float x = 500;
+float y = 500;
+float frequency = 1.0f;
+float amplitude = 70;
+
+int minuteCounter = 0;
+float secondCounter = 0;
+
+void Update() 
+{
+    dt = 1.0f / TARGET_FPS;
+    time += dt;
+
+    x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+    y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+
+    minuteCounter = time / 60;
+    secondCounter = time - (minuteCounter * 60);
+}
+
+void Draw()
+{
+    BeginDrawing();
+    ClearBackground(BLUE);
+    //DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+
+    GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    DrawText(TextFormat("Time: %.2f", time), GetScreenWidth() - 200, 20, 20, LIGHTGRAY);
+
+    DrawCircle(x, y, 60, RED);
+    DrawCircle(GetScreenWidth() / 2 + cos(time * frequency) * amplitude, GetScreenHeight() / 2, 60, GREEN);
+    DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2 + sin(time * frequency) * amplitude, 60, DARKPURPLE);
+
+    EndDrawing();
+}
+
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
+    InitWindow(InitialWidth, InitialHeight, "Cameron's Very Own Physics Engine");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
-        EndDrawing();
+        Update();
+        Draw();
     }
 
     CloseWindow();
